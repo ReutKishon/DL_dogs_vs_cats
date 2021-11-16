e9269d2b870a3cb198c335949117d15d143a9234
reutKeyshawn <reki8611@gmail.com>
2021-11-16 16:11:45 +0200

    load data done

--- cnn.py
+++ cnn.py
@@ -1,45 +1,95 @@
 # Import libraries
 import os
 import numpy as np
-import matplotlib.pyplot as plt
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
-if __name__ == "__main__":
-    batch_size = 32
-    img_height = 28
-    img_width = 28
-
-    train_ds = tf.keras.utils.image_dataset_from_directory(
-        'dataset/training_set/',
-        validation_split=0.2,
-        subset="training",
-        seed=123,
-        image_size=(img_height, img_width),
-        batch_size=batch_size)
-
-    val_ds = tf.keras.utils.image_dataset_from_directory(
-        'dataset/training_set/',
-        validation_split=0.2,
-        subset="validation",
-        seed=123,
-        image_size=(img_height, img_width),
-        batch_size=batch_size)
-
-    class_names = train_ds.class_names
-
-    print(class_names[0])
-    images = train_ds.take(1)
-    y = [y for x, y in train_ds]
-    print(y)
+import pickle
 
 
-# print(class_names_val)
+pickle_in = open("X.pickle", "rb")
+X = pickle.load(pickle_in)
 
-# plt.figure(figsize=(10, 10))
-# for images, labels in train_ds.take(1):
-#     for i in range(9):
-#         ax = plt.subplot(3, 3, i + 1)
-#         plt.imshow(images[i].numpy().astype("uint8"))
-#         plt.title(class_names[labels[i]])
-#         plt.axis("off")
-#         plt.show()
+pickle_in = open("y.pickle", "rb")
+y = pickle.load(pickle_in)
+
+print(y)
+
+
+n_classes = 2
+batch_size = 128
+
+x = tf.compat.v1.placeholder('float', [None, 784])
+y = tf.compat.v1.placeholder('float')
+
+keep_rate = 0.8
+keep_prob = tf.compat.v1.placeholder(tf.float32)
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def maxpool2d(x):
+    #                        size of window         movement of window
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+def convolutional_neural_network(x):
+    weights = {'W_conv1': tf.Variable(tf.random.normal([5, 5, 1, 32])),
+               'W_conv2': tf.Variable(tf.random.normal([5, 5, 32, 64])),
+               'W_fc': tf.Variable(tf.random.normal([7*7*64, 1024])),
+               'out': tf.Variable(tf.random.normal([1024, n_classes]))}
+
+    biases = {'b_conv1': tf.Variable(tf.random.normal([32])),
+              'b_conv2': tf.Variable(tf.random.normal([64])),
+              'b_fc': tf.Variable(tf.random.normal([1024])),
+              'out': tf.Variable(tf.random.normal([n_classes]))}
+
+    x = tf.reshape(x, shape=[-1, 28, 28, 1])
+
+    conv1 = tf.nn.relu(conv2d(x, weights['W_conv1']) + biases['b_conv1'])
+    conv1 = maxpool2d(conv1)
+
+    conv2 = tf.nn.relu(conv2d(conv1, weights['W_conv2']) + biases['b_conv2'])
+    conv2 = maxpool2d(conv2)
+
+    fc = tf.reshape(conv2, [-1, 7*7*64])
+    fc = tf.nn.relu(tf.matmul(fc, weights['W_fc'])+biases['b_fc'])
+    fc = tf.nn.dropout(fc, keep_rate)
+
+    output = tf.matmul(fc, weights['out'])+biases['out']
+
+    return output
+
+
+def train_neural_network(x):
+    prediction = convolutional_neural_network(x)
+
+    def cost():
+        return tf.math.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
+    optimizer = tf.keras.optimizers.Adam().minimize(cost, var_list=[])
+
+    hm_epochs = 10
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+
+        for epoch in range(hm_epochs):
+            epoch_loss = 0
+            for _ in range(int(mnist.train.num_examples/batch_size)):
+                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+                _, c = sess.run([optimizer, cost], feed_dict={
+                    x: epoch_x, y: epoch_y})
+                epoch_loss += c
+
+            print('Epoch', epoch, 'completed out of',
+                  hm_epochs, 'loss:', epoch_loss)
+
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Accuracy:', accuracy.eval(
+            {x: mnist.test.images, y: mnist.test.labels}))
+
+
+train_neural_network(x)
